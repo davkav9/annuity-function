e1d8fe12e71c2504136aca714906d228a778baa0
davkav9 <46248927+davkav9@users.noreply.github.com>
2019-02-05 17:36:27 +0000

    Add files via upload

--- annuity.py
+++ annuity.py
@@ -9,15 +9,15 @@ def annuity(term, interest, adv_arr, adv_arr_str, adv_arr_out, pv_av, pv_av_str,
     
     
     sum = 0
-    for i in range(1,term+1):
-    	sum += (1/((1+interest)**((i - adv_arr))/freq))/freq
+    for i in range(1,(term*freq)+1):
+        sum += (1/((1+interest)**((i - adv_arr)/freq)))/freq
 
     if input_pv_av == 1:
-    	sum *= (1+interest)**term
+        sum *= (1+interest)**term
 
         
     
-    print(f'The {pv_av_out} of a {term}-year annuity, payable in {adv_arr_out} at a rate of interest of {interest*100}% is: {sum}')
+    print(f'The {pv_av_out} of a {term}-year annuity, payable every {12/freq} months, in {adv_arr_out} at a rate of interest of {interest*100}% is: {sum}')
 
 #
 #    Set up inital variables
@@ -29,23 +29,45 @@ input_pv_av = 0
 output_adv_arr = 'arrears'
 output_pv_av = 'present value'
 
+
+
 while True:
     
+    print('\n' * 10)
+    print('ANNUITY CALCULATOR')
+    
     input_interest = int(input('\nPlease state the interest rate in (percent): '))/100
+
     input_term = int(input('\nPlease state the term (in years): '))
-    input_adv_arr_str = input("\nIs the annuity payable in advance or in arrears? (type 'adv' or 'arr')")
-    input_pv_av_str = input("\nAre you looking for the accumulated value or the present value? (type 'pv' or 'av')")
-    input_freq = int(input('\nHow often will this annuity be paid? (eg enter 2 for half-yearly, 4 for quarterly, 12 for monthly etc) '))
 
+    while True:
+        input_adv_arr_str = input("\nIs the annuity payable in advance or in arrears? (type 'adv' or 'arr')")
+        if input_adv_arr_str not in ['adv','arr']:
+            print("Sorry, please enter either 'adv' or 'arr'")
+            continue
+        else:
+            break
+        
+    while True:
+        input_pv_av_str = input("\nAre you looking for the accumulated value or the present value? (type 'pv' or 'av')")
+        if input_pv_av_str not in ['pv','av']:
+            print("Sorry, please enter either 'pv' or 'av'")
+            continue
+        else:
+            break
 
-    if input_adv_arr_str == 'adv':
-        input_adv_arr = 1
-        output_adv_arr = 'advance'
+    while True:
+	    try:
+	    	input_freq = int(input('\nHow often will this annuity be paid? (eg enter 2 for half-yearly, 4 for quarterly, 12 for monthly etc) '))
+	    except ValueError:
+	    	print('Sorry, please enter a number')
+	    	continue
+	    else:
+	    	break
 
     if input_pv_av_str == 'av':
         input_pv_av = 1
         output_pv_av = 'accumulated value'
-
 
     #
     #   Run function
